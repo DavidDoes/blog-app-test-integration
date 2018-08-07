52a06c9b7ac42708a2e57898c5e81d0b9c6fea77
David Clark <dlc.clark@gmail.com>
2018-08-06 19:54:52 -0700

    add PUT test

--- test/test.js
+++ test/test.js
@@ -19,22 +19,17 @@ function seedBlogPostData(){
     const seedData = [] //empty array to populate
 
     for (let i=1; i<=10; i++){ //populate empty arr w/ Faker documents
-      seedData.push(generateBlogPostData()) 
+      seedData.push({
+        author: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName()
+      },
+        title: faker.lorem.sentence(),
+        content: faker.lorem.paragraph()
+      })
     }
     //return Promise
     return BlogPost.insertMany(seedData)
-}
-
-//seed documents created above with Faker data
-function generateBlogPostData(){
-    return {
-        title: faker.lorem.sentence(),
-        content: faker.lorem.paragraph(),
-        author: {
-            firstName: faker.name.firstName(),
-            lastName: faker.name.lastName()
-        }
-    }
 }
 
 //delete entire db after each test
@@ -82,30 +77,29 @@ describe('BlogPosts API resource', function(){
 
         it('should return blogposts with correct fields', function(){
           //get all blogposts, ensure expected keys
-          let resBlogPost
+          let resPost
           return chai.request(app)
             .get('/posts')
             .then(function(res){
               expect(res).to.have.status(200)
               expect(res).to.be.json
-              expect(res.body.posts).to.be.a('array')
+              expect(res.body).to.be.a('array')
+              expect(res.body).to.have.lengthOf.at.least(1)
 
-              res.body.posts.forEach(function(blogpost){
+              res.body.forEach(function(blogpost){
                 expect(blogpost).to.be.a('object')
                 expect(blogpost).to.include.keys(
-                  'id', 'title', 'author', 'content', 'created'
+                  'id', 'title', 'author', 'content'
                 )
               })
-              resBlogPost = res.body.posts[0]
-              return BlogPost.findById(resBlogPost.id)
+              resPost = res.body[0]
+              return BlogPost.findById(resPost.id)
             })
             .then(function(blogpost){
               //expect values in blogpost object to be same as in db
-              expect(resBlogPost.id).to.equal(blogpost.id)
-              expect(resBlogPost.title).to.equal(blogpost.title)
-              expect(resBlogPost.content).to.equal(blogpost.content)
-              expect(resBlogPost.created).to.equal(blogpost.created)
-              expect(resBlogPost.author).to.equal(blogpost.author)
+              expect(resPost.title).to.equal(blogpost.title)
+              expect(resPost.content).to.equal(blogpost.content)
+              expect(resPost.author).to.equal(blogpost.authorName)
             })
         })
     })
@@ -113,7 +107,14 @@ describe('BlogPosts API resource', function(){
     describe('POST endpoint', function(){
       //create new blogpost, prove has correct keys
       it ('should add new blogpost', function(){
-        const newBlogPost = generateBlogPostData()
+        const newBlogPost = {
+          author: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName()
+          },
+          title: faker.lorem.sentence(),
+          content: faker.lorem.text()
+        }
         
         return chai.request(app)
           .post('/posts')
@@ -125,18 +126,55 @@ describe('BlogPosts API resource', function(){
             expect(res.body).to.include.keys(
               'id', 'title', 'content', 'author'
             )
-            expect(res.body.name).to.equal(newBlogPost.name)
             expect(res.body.id).to.not.be.null
             expect(res.body.title).to.equal(newBlogPost.title)
             expect(res.body.content).to.equal(newBlogPost.content)
-            // expect(res.body.author).to.equal(newBlogPost.author)
+            expect(res.body.author).to.be.equal(
+              `${newBlogPost.author.firstName} ${newBlogPost.author.lastName}`
+            )
+            return BlogPost.findById(res.body.id)
           })
           .then(function(blogpost){
             expect(blogpost.title).to.equal(newBlogPost.title)
             expect(blogpost.content).to.equal(newBlogPost.content)
-            // expect(blogpost.author).to.equal(newBlogPost.author)
             expect(blogpost.author.firstName).to.equal(newBlogPost.author.firstName)
             expect(blogpost.author.lastName).to.equal(newBlogPost.author.lastName)
+          })
+      })
+    })
+
+    describe('PUT endpoint', function(){
+      // 1. get existing blogpost
+      // 2. make PUT req to update that post
+      // 3. prove post returned contains data we sent
+      // 4. prove post in db correctly updated
+      it ('should update fields sent', function(){
+        const updateData = {
+          title: 'lorem',
+          content: 'ipsum',
+          author: {
+            firstName: 'Lorn',
+            lastName: 'Ipson'
+          }
+        }
+        return BlogPost
+          .findOne()
+          .then(function(blogpost){
+            updateData.id = blogpost.id
+            //get and ensure same as data sent
+            return chai.request(app)
+              .put(`/posts/${blogpost.id}`)
+              .send(updateData)
+          })
+          .then(function(res){
+            expect(res).to.have.status(204)
+            return BlogPost.findById(updateData.id)
+          })
+          .then(function(blogpost){
+            expect(blogpost.title).to.equal(updateData.title)
+            expect(blogpost.content).to.equal(updateData.content)
+            expect(blogpost.author.firstName).to.equal(updateData.author.firstName)
+            expect(blogpost.author.lastName).to.equal(updateData.author.lastName)
           })
       })
     })
